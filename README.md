370b86980b6c45b029b4e8e401452046bac5c8c2
Ztaclie <melmeh42@gmail.com>
2024-01-21 12:58:47 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Vite + React (Material Tailwind) Setup
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "@material-tailwind/react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Button</Button>
+    </div>
+  );
+}
+
+export default App;
